8c3f7c2b5830691996dd46f67c363884154869b4
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-11 16:31:20 +0700

    ข้อ2

--- 6320500697-5.c
+++ 6320500697-5.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 int main ()
 {
-    int i,k,n;
+    int i,k,n,j;
     scanf ("%d",&n);
+
     if (n%2==0)
     {
+        j=n/2;
+        j=j-1;
         for (i=0;i<n;i++)
         {
             for (k=0;k<n-1;k++)
             {
-                printf ("_");
+                 if(i==0||i==n-1)
+                {
+                    if (k==j)
+                    {
+                        printf ("*");
+                    }
+                    else
+                    {
+                        printf ("_");
+                    }
+                }
+                else
+                {
+                     printf ("_");
+                }
             }
              printf ("\n");
         }
     }
     else
     {
+
         for (i=0;i<n;i++)
         {
             for (k=0;k<n;k++)
             {
-                printf ("_");
+               printf ("_");
             }
             printf ("\n");
         }
