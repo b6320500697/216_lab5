fb405e743c26248f57a927e6048b35b85b9d0fe7
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-11 15:24:39 +0700

    Guly1

--- 6320500697-3.c
+++ 6320500697-3.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main ()
 {
-    int n,m,i,k;
+    int n,m,i,k=1;
     scanf ("%d",&n);
     for (i=1; ;i++)
     {
-        if(i%2==0||i%3==0||i%5==0)
+        if(i%2==0&&i%3==0||i%2==0&&i%5==0||i%3==0&&i%5==0||i%3==0)
         {
-
+            k++;
+        }
+        if (k==n)
+        {
+            break;
         }
     }
+    printf ("%d",i);
 }
